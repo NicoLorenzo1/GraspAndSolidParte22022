8e8b5db935fb4fd2c02bc432b5764c9335ce1e6f
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-04-21 11:54:51 -0300

    comentarios realizados

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 namespace Full_GRASP_And_SOLID.Library
 {
+    /// <summary>
+    /// Se crea la clase ConsolePrinter con la finalidad de aplicar los patrones Expert Y SRP. La clase "Recipe" tenía asignada la funcion 
+    /// de imprimir la receta, y no debía ser así ya que estaba haciendo dos funciones totalmente diferentes, por lo tanto se
+    /// crea Esta clase la cual solamente se encarga de imprimir la receta por consola y conoce toda la informacón necesaria para cumplir su función.
+    /// Por otro lado también es muy util ya que si en un futuro se quiere cambiar la forma de impresión o mostrar por consola se puede hacer facilmente modificando 
+    /// solamente esta clase y no afectando al resto del codigo a grandes rasgos (aplicando SRP).
+    /// </summary>
     public class Consoleprinter
     {
         public static void PrintRecipe()
